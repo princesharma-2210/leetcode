17950dc48374a4c32a620cb03b2e603b1f6d8ac1
Prince Sharma <shaarmprince@gmail.com>
2025-08-23 01:07:39 +0530

    Time: 1 ms (100%), Space: 57.2 MB (67.16%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        boolean arr[]= new boolean[nums.length+1];
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>0&& nums[i]<=nums.length ){
+                arr[nums[i]]=true;
+            }
+        }
+        for(int i=1; i<arr.length; i++){
+            if(!arr[i]){
+                return i;
+            }
+        }
+        return arr.length;
+    }
+}
